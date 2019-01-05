2304769a079e4bb9383d102abdb44ad9f1de6f3b
gotti <spacelyjoe@gmail.com>
2019-01-05 17:33:50 -0500

    cleaned up some code

--- src/core/Channel/Messages.ts
+++ src/core/Channel/Messages.ts
@@ -18,40 +18,9 @@ enum MSG_CODES {
     PATCH_STATE,
 };
 
-export const PROTOCOL_STRINGS  = {
-    [MSG_CODES.CONNECT]: 'CONNECT',
-    [MSG_CODES.DISCONNECT]: 'DISCONNECT',
-    [MSG_CODES.SEND_QUEUED]: 'SEND_QUEUED',
-    [MSG_CODES.SEND_BACK]: 'SEND_BACK',
-    [MSG_CODES.BROADCAST_ALL_BACK]: 'BROADCAST_ALL_BACK',
-
-    // BACK -> FRONT
-    [MSG_CODES.CONNECT_SUCCESS]: 'CONNECT_SUCCESS',
-    [MSG_CODES.CONNECT_FAILED]: 'CONNECT_FAILED',
-    [MSG_CODES.BROADCAST_MIRROR_FRONTS]: 'BROADCAST_MIRROR_FRONTS',
-    [MSG_CODES.BROADCAST_ALL_FRONTS]: 'BROADCAST_ALL_FRONTS',
-    [MSG_CODES.SEND_FRONT]: 'SEND_FRONT',
-    [MSG_CODES.SET_STATE]: 'SET_STATE',
-    [MSG_CODES.PATCH_STATE]: 'PATCH_STATE',
-};
-
 export type PublishProtocol = any;
 export type SubscribeProtocol = any;
 type SubscriptionHandler = (...any) => void;
-/*
-export interface PublishProtocol  {
-    (...any): any,
-    createPublish: Function,
-    removePublish: Function,
-    to?: Object<string, Function>,
-}
-
-export interface SubscribeProtocol {
-    createSubscribe: Function,
-    removeSubscribe: Function,
-}
-*/
-
 
 /**
  * helper class with functions to make sure protocol codes stay synchronized between front and back channels.
@@ -61,28 +30,18 @@ export class Protocol {
 
     // FRONT -> BACK
     static CONNECT() : string  { return Protocol.make(MSG_CODES.CONNECT) };
-
     static BROADCAST_ALL_BACK() : string  { return Protocol.make(MSG_CODES.BROADCAST_ALL_BACK) };
-
     static DISCONNECT(backChannelId) : string  { return Protocol.make(MSG_CODES.DISCONNECT, backChannelId) };
-
     static SEND_BACK(backChannelId) : string  { return Protocol.make(MSG_CODES.SEND_BACK, backChannelId) };
-
     static SEND_QUEUED(frontUid) : string  { return Protocol.make(MSG_CODES.SEND_QUEUED, frontUid) };
 
     // BACK -> FRONT
     static CONNECT_SUCCESS(frontUid) : string { return Protocol.make(MSG_CODES.CONNECT_SUCCESS, frontUid) };
-
     static CONNECT_FAILED(frontUid) : string { return Protocol.make(MSG_CODES.CONNECT_FAILED, frontUid) };
-
     static SEND_FRONT(frontUid) : string  { return Protocol.make(MSG_CODES.SEND_FRONT, frontUid) };
-
     static BROADCAST_MIRROR_FRONTS(frontChannelId) : string  { return Protocol.make(MSG_CODES.BROADCAST_MIRROR_FRONTS, frontChannelId) };
-
     static SET_STATE(frontChannelId): string  { return Protocol.make(MSG_CODES.SET_STATE, frontChannelId) };
-
     static PATCH_STATE(frontChannelId) : string  { return Protocol.make(MSG_CODES.PATCH_STATE, frontChannelId) };
-
     static BROADCAST_ALL_FRONTS() : string  { return Protocol.make(MSG_CODES.BROADCAST_ALL_FRONTS) };
 
     /**
@@ -194,5 +153,3 @@ export abstract class ChannelMessages {
         return sub;
     }
 }
-
-export default Protocol;
